231b7ee1ac9ce1b089b1441fa22b21aab5ad5886
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-04-27 13:10:55 +0530

    implement persistence using AsyncStorage

--- context/authProvider.jsx
+++ context/authProvider.jsx
@@ -1,16 +1,17 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../config/firebase";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AuthContext = createContext();
+const debug = false;
 
 export const AuthProvider = ({ children }) => {
   // ! states section
-  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
   const [isLogged, setIsLogged] = useState(false);
-  const [isEmailUser, setIsEmailUser] = useState(false);
-  const [isGoogleUser, setIsGoogleUser] = useState(false);
+  // const [isEmailUser, setIsEmailUser] = useState(false);
+  // const [isGoogleUser, setIsGoogleUser] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, initializeUser);
@@ -20,27 +21,49 @@ export const AuthProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
-  const initializeUser = (user) => {
+  const initializeUser = async (user) => {
     if (user) {
-      setUser({ ...user });
-
-      // ! check for email login
-      const isEmail = user.providerData.some(
-        (provider) => provider.providerId === "password"
-      );
-
-      setIsEmailUser(isEmail);
-      setIsLogged(true);
+      debug && console.log("user data");
+      debug && console.log(user);
+      await AsyncStorage.setItem("@AuthData", JSON.stringify(user));
     } else {
-      setUser(null);
-      setIsLogged(false);
-    }
+      const result = await AsyncStorage.getItem("@AuthData");
 
-    setIsLoading(false);
+      if (result) {
+        debug && console.log("Logout scenario");
+        await AsyncStorage.removeItem("@AuthData");
+        setUser(null);
+        setIsLogged(false);
+      }
+    }
   };
 
+  const fetchData = async () => {
+    try {
+      const result = await AsyncStorage.getItem("@AuthData");
+
+      if (result) {
+        const parsedResult = JSON.parse(result);
+        debug && console.log("User set");
+        setUser(parsedResult);
+        setIsLogged(true);
+      } else {
+        debug && console.log("User set to null");
+        setUser(null);
+        setIsLogged(false);
+      }
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const value = {
-    isLoading,
     user,
     setUser,
     isLogged,
